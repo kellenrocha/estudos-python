76d1937bf97e28ed580bf67114707a62550f1c09
kellen rocha <kjr.silva@hotmail.com>
2025-02-26 14:54:28 -0300

    Primeiro commit - subindo meus estudos em Pyhton

---
+++ day1.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+
+
+#isso é um comentário
+
+print(1*5)
+print("1*5")
+print(3)
+
+# %%
+print("teste")
+# %%
+
+
+# pra rodar no terminal é só digitar: python nome_do_arquivo.py
+
+x=5
+y=10
+print(x + y) #não funcionou
