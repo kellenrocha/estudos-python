f33f4e8e513c194a0ee7cbc26e58b5502ed235d9
kellen rocha <kjr.silva@hotmail.com>
2025-07-07 23:10:54 -0300

    Calculadora em python - adição

--- laboratorios-praticas/calculadora.py
+++ laboratorios-praticas/calculadora.py
@@ -1,3 +1,23 @@
+def Soma():
+    lista_elementos = []
+    elementos = int(input('Digite a quantidade de elementos da Soma: '))
+    
+    
+    if elementos < 2:
+        print('Para realizar a operação é necessário mais de um valor... encerrando ')
+    else:
+        for i in range(1, elementos + 1):
+            lista_elementos.append(int(input(f'Digite o {i} º valor: ')))
+
+        Tsoma = 0
+        for x in lista_elementos:
+            Tsoma = Tsoma + x
+        print('Total da Soma: ', Tsoma)
+
+
+#--------------------------------------------------------------------------------------
+
+
 print()
 print("******************* Calculadora em Python *******************")
 print()
@@ -10,12 +30,7 @@ operacao = int(input('Digite sua opção (1/2/3/4): '))
 #elementos = input('Digite a quantidade de elementos da operação: ')
 
 if operacao == 1:
-    print('Soma')
-    lista_elementos = []
-    elementos = int(input('Digite a quantidade de elementos da operação: '))
-    for i in range(1, elementos + 1):
-        lista_elementos.append(input(f'Digite o {i} º valor: '))
-    #print(lista_elementos)
+    Soma() 
 elif operacao == 2:
     print('Subtração')
 elif operacao == 3: 
@@ -24,3 +39,5 @@ elif operacao == 4:
     print('Divisão')
 else: 
     print('Entrada inválida')
+
+
